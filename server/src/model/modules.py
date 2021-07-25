82b8aac3719a72b4a05d97037cfeae5347df0eec
jose <jose.chavez.alvarez@ucsp.edu.pe>
2021-07-24 20:34:17 -0500

    feat(model): decode, split_label, and test

--- server/src/model/modules.py
+++ server/src/model/modules.py
@@ -1,13 +1,15 @@
+import os
 import torch
 import torch.nn as nn
 import numpy as np
+import base64
 
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
-from PIL import Image
 
 import torchvision.transforms as transforms
 
+import network
 
 def ConvBlock(in_channels, out_channels, pool=False):
     layers = [
@@ -18,38 +20,11 @@ def ConvBlock(in_channels, out_channels, pool=False):
         layers.append(nn.MaxPool2d(4))
     return nn.Sequential(*layers)
 
-class ResNet9(nn.Module):
-    def __init__(self, in_channels, num_diseases):
-        super().__init__()
-        
-        self.conv1 = ConvBlock(in_channels, 64)
-        self.conv2 = ConvBlock(64, 128, pool=True) # out_dim : 128 x 64 x 64 
-        self.res1 = nn.Sequential(ConvBlock(128, 128), ConvBlock(128, 128))
-        
-        self.conv3 = ConvBlock(128, 256, pool=True) # out_dim : 256 x 16 x 16
-        self.conv4 = ConvBlock(256, 512, pool=True) # out_dim : 512 x 4 x 44
-        self.res2 = nn.Sequential(ConvBlock(512, 512), ConvBlock(512, 512))
-        
-        self.classifier = nn.Sequential(nn.MaxPool2d(4),
-                                       nn.Flatten(),
-                                       nn.Linear(512, num_diseases))
-        
-    def forward(self, x):
-        out = self.conv1(x)
-        out = self.conv2(out)
-        out = self.res1(out) + out
-        out = self.conv3(out)
-        out = self.conv4(out)
-        out = self.res2(out) + out
-        out = self.classifier(out)
-        return out
-
 def create_network(device):
-    model = ResNet9(3, 38).to(device)
+    model = network.ResNet9(3, 38).to(device)
     return model
 
 def load_model(device, load_path):
-    from torchvision.datasets import ImageFolder
     model = create_network(device)
     checkpoint = torch.load(load_path)
     model.load_state_dict(checkpoint)
@@ -65,28 +40,36 @@ def load_test_db(path):
 
     return test_dl, len(test_db)
 
-def eval_test_db(model, path, device):
-    data_generator, data_size = load_test_db(path)
-
+def eval_accuracy(model, path, device):
     model.eval()
-
+    data_generator, data_size = load_test_db(path)
     acc = []
     for it, batch in enumerate(data_generator):
         image_batch, label_batch = batch
         y = model(image_batch.to(device))
         acc_it = (torch.max(y.detach().cpu(), dim=1)[1] == label_batch)
-        acc.append(acc_it)
-        print('\r[{:4d}/{:4d}] acc = {:3.2f}'.format(it, data_size, acc_it.item()))
-    return np.mean(acc)
+        acc.append(float(acc_it.item()))
+        print('\r[{:4d}/{:4d}] acc = {:3.5f}%'.format(
+            it, data_size, np.mean(acc) * 100.), end='')
+    accuracy = np.mean(acc)
+    print('\r[{:4d}/{:4d}] acc = {:3.5f}%'.format(
+        it, data_size, accuracy * 100.))
+    return accuracy
 
-def predict_image(filename, model, classes_id, device):
-    img = Image.open(filename)
-    x = torch.Tensor(img).unsqueeze(0).to(device)
-    y = model(x)
-    _, preds  = torch.max(y, dim=1)
-    return classes_id[preds[0].item()]
+def test_classes_ids():
+    test_path = 'dataset/test'
 
-def predict_db(model, path, device):
+    test1 = sorted(os.listdir(test_path))
+    test2 = get_db(test_path)[1]
+    for t1, t2 in zip(test1, test2):
+        assert t1 == t2
+
+def accuracy_performance(path, device):
+    model = load_model(device, os.path.join('params', 'model.pth'))
+    assert eval_accuracy(model, path, device) >= 0.95
+
+def predict_random_image(path, device):
+    model = load_model(device, os.path.join('params', 'model.pth'))
     model.eval()
     test_db, classes_id = get_db(path)
     x, pred_y = test_db[np.random.randint(len(test_db))]
@@ -95,5 +78,6 @@ def predict_db(model, path, device):
     _, pred_y_hat  = torch.max(y_hat, dim=1)
 
     return classes_id[pred_y], classes_id[pred_y_hat[0].item()]
+
 
 
