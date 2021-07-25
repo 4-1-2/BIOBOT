c0fc618baa07817335ace8859525f0288b1f126b
erickTornero <erickdeivy01@gmail.com>
2021-07-25 17:51:12 -0500

    short fix credentials openai in cfignore

--- server/rest_app.py
+++ server/rest_app.py
@@ -64,4 +64,5 @@ api.add_resource(Diagnosis, '/diagnosis')
 api.add_resource(ChatBot, '/chatbot')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.getenv('PORT', 8000))
+    app.run(host='0.0.0.0', port=port, debug=False)
